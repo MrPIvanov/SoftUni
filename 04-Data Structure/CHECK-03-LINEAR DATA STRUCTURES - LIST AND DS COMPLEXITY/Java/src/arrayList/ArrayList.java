631014b700c696451c7ad38f5751ddd172108f67
MrPIvanov <MrPIvanov1117@gmail.com>
2018-01-14 15:39:55 +0200

    Data Structure Lists.

---
+++ 04-Data Structure/CHECK-03-LINEAR DATA STRUCTURES - LIST AND DS COMPLEXITY/Java/src/arrayList/ArrayList.java
@@ -0,0 +1,36 @@
+package arrayList;
+
+public class ArrayList<T>{
+	
+	public ArrayList(int capacity){
+		
+	}
+	
+	public ArrayList(){
+	}
+
+	
+	public int getCount() {
+		return 0;
+	}
+
+
+	public T get(int index) {
+		return null;
+	}
+
+
+	public void add(T element) {
+
+	}
+
+	public T removeAt(int index) {
+		return null;
+	}
+	
+
+	public void set(int i, T item) {
+
+	}
+	
+}
