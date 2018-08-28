a11778fe8c0bf5c8040fe07de1fbefe82d171705
MrPIvanov <MrPIvanov1117@gmail.com>
2018-08-28 15:22:03 +0300

    Basic CRUD Exercises.

---
+++ 08-Database Basics – MS SQL/06-EXERCISE BASIC CRUD/BasicCrudExercises/_17_CreateViewEmployeesWithJobTitles.sql
@@ -0,0 +1,10 @@
+USE SoftUni
+GO
+
+CREATE VIEW V_EmployeeNameJobTitle AS
+SELECT FirstName + ' ' + ISNULL(MiddleName,'') + ' ' + LastName AS [Full Name], JobTitle
+  FROM Employees
+GO
+
+SELECT *
+  FROM V_EmployeeNameJobTitle
---
+++ 08-Database Basics – MS SQL/06-EXERCISE BASIC CRUD/BasicCrudExercises/_21_IncreaseSalaries.sql
@@ -0,0 +1,12 @@
+USE SoftUni
+
+BEGIN TRANSACTION
+
+UPDATE Employees
+  SET Salary*=1.12
+WHERE DepartmentID IN(1, 2, 4, 11)
+
+SELECT Salary
+  FROM Employees
+
+ROLLBACK TRANSACTION
