54f0146880e5b5f0ab9c26a85ad002d27231bf60
MrPIvanov <MrPIvanov1117@gmail.com>
2018-12-27 11:34:53 +0200

    Linear Data Structures Exercises.

---
+++ 08-Data Structure/03-CHECK-LINEAR DS - LIST AND DS COMPLEXITY/LinearDataStructures_Exercises_01_07/_03_LongestSubsequence/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_LongestSubsequence
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var maxNumbersSequence = 0;
+            var maxNumberToPrint = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var currentSequence = 1;
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        currentSequence++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (currentSequence > maxNumbersSequence)
+                {
+                    maxNumbersSequence = currentSequence;
+                    maxNumberToPrint = numbers[i];
+                }
+            }
+
+            for (int i = 0; i < maxNumbersSequence; i++)
+            {
+                Console.Write($"{maxNumberToPrint} ");
+            }
+        }
+    }
+}
---
+++ 08-Data Structure/03-CHECK-LINEAR DS - LIST AND DS COMPLEXITY/LinearDataStructures_Lab/Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var testArray = new ArrayList<int>();
+
+        testArray.Add(1);
+        testArray.Add(2);
+        testArray.Add(3);
+        testArray.Add(4);
+
+
+
+        testArray.RemoveAt(2);
+
+
+        System.Console.WriteLine();
+    }
+}
