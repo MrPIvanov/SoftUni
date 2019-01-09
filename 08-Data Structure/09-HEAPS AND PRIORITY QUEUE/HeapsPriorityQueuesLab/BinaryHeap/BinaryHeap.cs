af3a7a1709084a992f8d18e1c7501744a1c6d885
MrPIvanov <MrPIvanov1117@gmail.com>
2019-01-09 13:06:45 +0200

    Heaps and Priority Queues Lab.

---
+++ 08-Data Structure/09-HEAPS AND PRIORITY QUEUE/HeapsPriorityQueuesLab/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.heap.Count;
+        }
+    }
+
+    public void Insert(T item)
+    {
+        this.heap.Add(item);
+        this.HeapifyUp(this.heap.Count - 1);
+    }
+
+    private void HeapifyUp(int ItemIndex)
+    {
+        var parrentIndex = (ItemIndex - 1) / 2;
+
+        var compare = this.heap[parrentIndex].CompareTo(this.heap[ItemIndex]);
+
+        if (compare < 0)
+        {
+            Swap(parrentIndex, ItemIndex);
+            ItemIndex = parrentIndex;
+            this.HeapifyUp(ItemIndex);
+        }
+    }
+
+    private void Swap(int parrentIndex, int itemIndex)
+    {
+        var temp = this.heap[parrentIndex];
+        this.heap[parrentIndex] = this.heap[itemIndex];
+        this.heap[itemIndex] = temp;
+    }
+
+    public T Peek()
+    {
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var element = this.heap[0];
+
+        Swap(0, this.Count - 1);
+
+        this.heap.RemoveAt(this.Count - 1);
+
+        this.HeapifyDown(0);
+
+        return element;
+    }
+
+    private void HeapifyDown(int itemIndex)
+    {
+        if (this.Count / 2 > itemIndex)
+        {
+            var childIndex = (2 * itemIndex) + 1;
+
+            if ((childIndex + 1) < this.Count - 1 &&
+                this.heap[childIndex].CompareTo(this.heap[childIndex + 1]) < 0)
+            {
+                childIndex++;
+            }
+
+            var compare = this.heap[itemIndex].CompareTo(this.heap[childIndex]);
+
+            if (compare < 0)
+            {
+                Swap(itemIndex, childIndex);
+
+                itemIndex = childIndex;
+
+                this.HeapifyDown(itemIndex);
+            }
+        }
+    }
+}
---
+++ 08-Data Structure/09-HEAPS AND PRIORITY QUEUE/HeapsPriorityQueuesLab/BinaryHeap/Heap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        var n = arr.Length;
+
+        for (int i = n / 2; i >= 0; i--)
+        {
+            MoveElementDown(arr, i, n);
+        }
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);
+            MoveElementDown(arr, 0, i);
+        }
+    }
+
+    public static void MoveElementDown(T[] arr, int itemIndex, int LastIndex)
+    {
+        if (LastIndex / 2 > itemIndex)
+        {
+            var childIndex = (2 * itemIndex) + 1;
+
+            if ((childIndex + 1) < LastIndex  &&
+                arr[childIndex].CompareTo(arr[childIndex + 1]) < 0)
+            {
+                childIndex++;
+            }
+
+            var compare = arr[itemIndex].CompareTo(arr[childIndex]);
+
+            if (compare < 0)
+            {
+                Swap(arr, itemIndex, childIndex);
+
+                itemIndex = childIndex;
+
+                MoveElementDown(arr, itemIndex, LastIndex);
+            }
+        }
+    }
+
+    private static void Swap(T[] arr, int currentItemIndex, int childIndex)
+    {
+        var temp = arr[currentItemIndex];
+        arr[currentItemIndex] = arr[childIndex];
+        arr[childIndex] = temp;
+    }
+}
