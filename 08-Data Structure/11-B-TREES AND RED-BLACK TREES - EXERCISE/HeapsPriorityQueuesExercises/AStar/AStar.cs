7973b80dc0c532e745e14c61fabf9910bb2c7ebc
MrPIvanov <MrPIvanov1117@gmail.com>
2019-01-15 10:25:38 +0200

    Heaps Red-Black Trees Exercises.

---
+++ 08-Data Structure/11-B-TREES AND RED-BLACK TREES - EXERCISE/HeapsPriorityQueuesExercises/AStar/AStar.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AStar
+{
+    private char[,] map;
+    private int mapRows;
+    private int mapColumns;
+
+    private PriorityQueue<Node> queue { get; set; }
+    private Dictionary<Node, Node> parent { get; set; }
+    private Dictionary<Node, int> cost { get; set; }
+
+    public AStar(char[,] map)
+    {
+        this.cost = new Dictionary<Node, int>();
+        this.parent = new Dictionary<Node, Node>();
+        this.queue = new PriorityQueue<Node>();
+        this.map = map;
+        this.mapRows = this.map.GetLength(0);
+        this.mapColumns = this.map.GetLength(1);
+    }
+
+    public static int GetH(Node current, Node goal)
+    {
+        var deltaY = Math.Abs(current.Row - goal.Row);
+        var deltaX = Math.Abs(current.Col - goal.Col);
+
+        return deltaY + deltaX;
+    }
+
+    public IEnumerable<Node> GetPath(Node start, Node goal)
+    {
+        this.queue.Enqueue(start);
+        this.parent[start] = null;
+        this.cost[start] = 0;
+
+        var steps = new int[4] { 1, -1, 0, 0 };
+
+        while (this.queue.Count > 0)
+        {
+            var current = this.queue.Dequeue();
+
+            if (current.Equals(goal))
+            {
+                break;
+            }
+
+            var currentRow = current.Row;
+            var currentCol = current.Col;
+
+            for (int i = 0, j = 3; i < 4; i++, j--)
+            {
+                var row = currentRow + steps[i];
+                var col = currentCol + steps[j];
+
+                if (IsValid(row, col))
+                {
+                    var neighbour = new Node(row, col);
+
+                    var newCost = cost[current] + 1;
+
+                    if (!this.cost.ContainsKey(neighbour) || newCost < this.cost[neighbour])
+                    {
+                        this.cost[neighbour] = newCost;
+                        neighbour.F = newCost + GetH(neighbour, goal);
+                        this.queue.Enqueue(neighbour);
+                        parent[neighbour] = current;
+                    }
+                }
+            }
+        }
+
+        return GetPath(parent, start, goal);
+    }
+
+    private bool IsValid(int row, int col)
+    {
+        return row >= 0 && row < this.mapRows
+               && col >= 0 && col < this.mapColumns
+               && this.map[row, col] != 'W';
+    }
+
+    private IEnumerable<Node> GetPath(Dictionary<Node, Node> parent, Node start, Node goal)
+    {
+        var path = new Stack<Node>();
+
+        if (!this.parent.ContainsKey(goal))
+        {
+            path.Push(start);
+            return path;
+        }
+
+        var current = goal;
+
+        while (current != null)
+        {
+            path.Push(current);
+            current = this.parent[current];
+        }
+
+        return path;
+    }
+}
+
---
+++ 08-Data Structure/11-B-TREES AND RED-BLACK TREES - EXERCISE/Hierarchy/Hierarchy.Core/Hierarchy.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class Hierarchy<T> : IHierarchy<T>
+{
+    private Node root;
+    private Dictionary<T, Node> allNodes;
+
+    public Hierarchy(T root)
+    {
+        this.root = new Node(root);
+        this.allNodes = new Dictionary<T, Node>();
+        allNodes.Add(root, this.root);
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.allNodes.Count;
+        }
+    }
+
+    public void Add(T element, T child)
+    {
+        if (!this.allNodes.ContainsKey(element))
+        {
+            throw new ArgumentException();
+        }
+
+        if (this.allNodes.ContainsKey(child))
+        {
+            throw new ArgumentException();
+        }
+
+        var parrentNode = this.allNodes[element];
+        var childNode = new Node(child, parrentNode);
+
+        parrentNode.Childrens.Add(childNode);
+        this.allNodes.Add(child, childNode);
+    }
+
+    public void Remove(T element)
+    {
+        if (!this.allNodes.ContainsKey(element))
+        {
+            throw new ArgumentException();
+        }
+
+        var nodeToRemove = this.allNodes[element];
+
+        if (nodeToRemove.Parrent == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var nodeParrent = nodeToRemove.Parrent;
+
+        foreach (var child in nodeToRemove.Childrens)
+        {
+            child.Parrent = nodeParrent;
+            nodeParrent.Childrens.Add(child);
+        }
+
+        nodeParrent.Childrens.Remove(nodeToRemove);
+        this.allNodes.Remove(nodeToRemove.Value);
+    }
+
+    public IEnumerable<T> GetChildren(T item)
+    {
+        if (!this.allNodes.ContainsKey(item))
+        {
+            throw new ArgumentException();
+        }
+
+        var node = this.allNodes[item];
+
+        var childrens = node.Childrens.Select(x => x.Value);
+
+        return childrens;
+    }
+
+    public T GetParent(T item)
+    {
+        if (!this.allNodes.ContainsKey(item))
+        {
+            throw new ArgumentException();
+        }
+
+        var node = this.allNodes[item];
+
+        if (node.Parrent == null)
+        {
+            return default(T);
+        }
+
+        return node.Parrent.Value;
+    }
+
+    public bool Contains(T value)
+    {
+        if (this.allNodes.ContainsKey(value))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerable<T> GetCommonElements(Hierarchy<T> other)
+    {
+        var result = new List<T>();
+
+        foreach (var value in this.allNodes.Keys)
+        {
+            if (other.Contains(value))
+            {
+                result.Add(value);
+            }
+        }
+
+        return result;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var queue = new Queue<Node>();
+
+        queue.Enqueue(this.root);
+
+        while (queue.Count > 0)
+        {
+            var currentNode = queue.Dequeue();
+
+            foreach (var child in currentNode.Childrens)
+            {
+                queue.Enqueue(child);
+            }
+
+            yield return currentNode.Value;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class Node
+    {
+        public T Value { get; set; }
+
+        public Node Parrent { get; set; }
+
+        public List<Node> Childrens { get; set; }
+
+        public Node(T value, Node parrent = null)
+        {
+            this.Value = value;
+            this.Parrent = parrent;
+            this.Childrens = new List<Node>();
+        }
+    }
+}
