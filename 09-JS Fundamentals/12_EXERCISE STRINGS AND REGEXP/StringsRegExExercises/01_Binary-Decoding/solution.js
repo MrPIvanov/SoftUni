adf35d049507614eef3f79239a364595b78e1513
MrPIvanov <MrPIvanov1117@gmail.com>
2019-02-06 15:10:49 +0200

    Strings And Regex Exercises.

--- 09-JS Fundamentals/12_EXERCISE STRINGS AND REGEXP/StringsRegExExercises/01_Binary-Decoding/solution.js
+++ 09-JS Fundamentals/12_EXERCISE STRINGS AND REGEXP/StringsRegExExercises/01_Binary-Decoding/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+
+  let textToDecode = document.getElementById('str').value;
+  let resultElement = document.getElementById('result');
+
+  let sum = textToDecode.split('').map(Number).reduce((a, b) => a + b, 0);
+
+  while (sum > 9) {
+    let textSum = String(sum);
+    sum = textSum.split('').map(Number).reduce((a, b) => a + b, 0);
+  }
+
+  let textToWork = textToDecode.slice(sum);
+  textToWork = textToWork.slice(0, (sum * -1));
+  let numbers = textToWork.match(/.{1,8}/g);
+
+  let chars = numbers.map(n => parseInt(n, 2));
+  let result = '';
+
+  for (const ch of chars) {
+    let charToAdd = String.fromCharCode(ch);
+    if (charToAdd.match(/[a-z]/i) || charToAdd === ' ') {
+      result += charToAdd;
+    }
+  }
+
+  resultElement.textContent = result;
 }
--- 09-JS Fundamentals/12_EXERCISE STRINGS AND REGEXP/StringsRegExExercises/02_Expedition-Problems/solution.js
+++ 09-JS Fundamentals/12_EXERCISE STRINGS AND REGEXP/StringsRegExExercises/02_Expedition-Problems/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-  //TODO...
+  let keyWord = document.getElementById('str').value;
+  let text = document.getElementById('text').value;
+
+  let messagePattern = new RegExp(keyWord + "(.+)" + keyWord);
+
+  let message = text.match(messagePattern)[1];
+
+  let coordinatesPattern = /(north|east).*?([0-9]{2})[^,]*?,[^,]*?([0-9]{6})/gmi;
+
+  let arrCoordinates = [];
+
+  while ((temp = coordinatesPattern.exec(text)) !== null) {
+    arrCoordinates.push(temp[1] + ' ' + temp[2] + '.' + temp[3]);
+  }
+
+  let nCord = '';
+  let eCord = '';
+
+  while (!nCord || !eCord) {
+
+    let current = arrCoordinates.pop();
+
+    if (current[0].toLowerCase() === 'n' && !nCord) {
+      nCord = current;
+    }
+
+    if (current[0].toLowerCase() === 'e' && !eCord) {
+      eCord = current;
+    }
+  }
+
+  let resultElement = document.getElementById('result');
+
+  CreateAndAppendElement(`${nCord.split(' ')[1]} N`);
+  CreateAndAppendElement(`${eCord.split(' ')[1]} E`);
+  CreateAndAppendElement(`Message: ${message}`);
+
+  function CreateAndAppendElement(text) {
+    let element = document.createElement('p');
+    element.textContent = text;
+    resultElement.appendChild(element);
+  }
 }
