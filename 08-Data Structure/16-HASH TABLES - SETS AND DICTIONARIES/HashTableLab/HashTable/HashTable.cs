6d8988dae6394846a1d3c90bc32782e8632b561e
MrPIvanov <MrPIvanov1117@gmail.com>
2019-02-15 09:54:44 +0200

    HashTable Lab.

--- 08-Data Structure/16-HASH TABLES - SETS AND DICTIONARIES/HashTableLab/HashTable/HashTable.cs
+++ 08-Data Structure/16-HASH TABLES - SETS AND DICTIONARIES/HashTableLab/HashTable/HashTable.cs
@@ -1,107 +1,207 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
 {
+    private const int DefaultCapacity = 17;
+    private const double FillFactor = 0.71;
+    private LinkedList<KeyValue<TKey, TValue>>[] data;
+
     public int Count { get; private set; }
 
     public int Capacity
     {
-        get
-        {
-            throw new NotImplementedException();
-        }
+        get => this.data.Length;
     }
 
-    public HashTable()
+    public HashTable(int capacity = DefaultCapacity)
     {
-        throw new NotImplementedException();
-    }
-
-    public HashTable(int capacity)
-    {
-        throw new NotImplementedException();
+        this.data = new LinkedList<KeyValue<TKey, TValue>>[capacity];
+        this.Count = 0;
     }
 
     public void Add(TKey key, TValue value)
     {
-        throw new NotImplementedException();
-        // Note: throw an exception on duplicated key
+        GrowIfNeeded();
+
+        var index = Math.Abs(key.GetHashCode()) % this.Capacity;
+
+        if (this.data[index] == null)
+        {
+            this.data[index] = new LinkedList<KeyValue<TKey, TValue>>();
+        }
+
+        foreach (var item in this.data[index])
+        {
+            if (item.Key.Equals(key))
+            {
+                throw new ArgumentException("Keyalready exists: " + key);
+            }
+        }
+
+        this.data[index].AddLast(new KeyValue<TKey, TValue>(key, value));
+        this.Count++;        
+    }
+
+    private void GrowIfNeeded()
+    {
+        var currentFill = (double)(this.Count + 1) / this.Capacity;
+        if (currentFill > FillFactor)
+        {
+            Grow();
+        }
+    }
+
+    private void Grow()
+    {
+        var newData = new HashTable<TKey, TValue>(this.Capacity * 2);
+
+        foreach (var linkedList in this.data.Where(x => x != null))
+        {
+            foreach (var item in linkedList)
+            {
+                newData.Add(item.Key, item.Value);
+            }
+        }
+
+        this.data = newData.data;
     }
 
     public bool AddOrReplace(TKey key, TValue value)
     {
-        throw new NotImplementedException();
+        GrowIfNeeded();
+
+        var index = Math.Abs(key.GetHashCode()) % this.Capacity;
+
+        if (this.data[index] == null)
+        {
+            this.data[index] = new LinkedList<KeyValue<TKey, TValue>>();
+        }
+
+        foreach (var item in this.data[index])
+        {
+            if (item.Key.Equals(key))
+            {
+                item.Value = value;
+                return false;
+            }
+        }
+
+        this.data[index].AddLast(new KeyValue<TKey, TValue>(key, value));
+        this.Count++;
+        return true;
     }
 
     public TValue Get(TKey key)
     {
-        throw new NotImplementedException();
-        // Note: throw an exception on missing key
+        var element = this.Find(key);
+
+        if (element == null)
+        {
+            throw new KeyNotFoundException();
+        }
+
+        return element.Value;
     }
 
     public TValue this[TKey key]
     {
-        get
-        {
-            throw new NotImplementedException();
-            // Note: throw an exception on missing key
-        }
-        set
-        {
-            throw new NotImplementedException();
-        }
+        get => this.Get(key);
+        set => this.AddOrReplace(key, value);
     }
 
     public bool TryGetValue(TKey key, out TValue value)
     {
-        throw new NotImplementedException();
+        var element = this.Find(key);
+        if (element == null)
+        {
+            value = default(TValue);
+            return false;
+        }
+        value = element.Value;
+        return true;
     }
 
     public KeyValue<TKey, TValue> Find(TKey key)
     {
-        throw new NotImplementedException();
+        var index = Math.Abs(key.GetHashCode()) % this.Capacity;
+
+        if (this.data[index] == null)
+        {
+            return null;
+        }
+
+        foreach (var item in this.data[index])
+        {
+            if (key.Equals(item.Key))
+            {
+                return item;
+            }
+        }
+
+        return null;
     }
 
     public bool ContainsKey(TKey key)
     {
-        throw new NotImplementedException();
+        var element = this.Find(key);
+        return element != null;
     }
 
     public bool Remove(TKey key)
     {
-        throw new NotImplementedException();
+        var index = Math.Abs(key.GetHashCode()) % this.Capacity;
+
+        if (this.data[index] != null)
+        {
+            var currentElement = this.data[index].First;
+            while (currentElement != null)
+            {
+                if (currentElement.Value.Key.Equals(key))
+                {
+                    this.data[index].Remove(currentElement);
+                    this.Count--;
+                    return true;
+                }
+
+                currentElement = currentElement.Next;
+            }
+        }
+
+        return false;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException();
+        this.data = new LinkedList<KeyValue<TKey, TValue>>[DefaultCapacity];
+        this.Count = 0;
     }
 
     public IEnumerable<TKey> Keys
     {
-        get
-        {
-            throw new NotImplementedException();
-        }
+        get => this.Select(x => x.Key);
     }
 
     public IEnumerable<TValue> Values
     {
-        get
-        {
-            throw new NotImplementedException();
-        }
+        get => this.Select(x => x.Value);
     }
 
     public IEnumerator<KeyValue<TKey, TValue>> GetEnumerator()
     {
-        throw new NotImplementedException();
+        foreach (var linkedList in this.data.Where(x => x != null))
+        {
+            foreach (var item in linkedList)
+            {
+                yield return item;
+            }
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        throw new NotImplementedException();
+        return this.GetEnumerator();
     }
 }
