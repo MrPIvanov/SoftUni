fa0dfd53b8ccd0c2d2b989b13c06c08957a10ef2
MrPIvanov <MrPIvanov1117@gmail.com>
2019-02-16 09:39:46 +0200

    HashTable Exercises.

---
+++ 08-Data Structure/17-QUAD  K-D  INTERVAL TREES, HASH TABLES - EXERCISE/HashTableExercises/02_Phonebook/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        string command;
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+        while ((command = Console.ReadLine()) != "search")
+        {
+            var tokens = command.Split('-');
+            phonebook.AddOrReplace(tokens[0], tokens[1]);
+        }
+
+        while ((command = Console.ReadLine()) != "end")
+        {
+            if (phonebook.ContainsKey(command))
+            {
+                Console.WriteLine($"{command} -> {phonebook.Get(command)}");
+            }
+            else
+            {
+                Console.WriteLine($"Contact {command} does not exist.");
+            }
+        }
+    }
+}
