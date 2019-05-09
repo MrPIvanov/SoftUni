0231e78a98563f305978faf4f782185bca0eb65a
MrPIvanov <MrPIvanov1117@gmail.com>
2019-05-09 16:39:45 +0300

    Combinatorial And Greedy Algorithms Exercises Files.

---
+++ 14-Algorithms/07_COMBINATORIAL AND GREEDY ALGORITHMS - EXERCISE/CombinatorialExercises/01_IterativePermutWithoutRepetitions/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+public class StartUp
+{
+    private static char[] arr;
+    private static int[] permSwappings;
+
+    public static void Main()
+    {
+        arr = Console.ReadLine()
+             .Split()
+             .Select(char.Parse)
+             .ToArray();
+
+        permSwappings = Enumerable.Range(0, arr.Length).ToArray();
+
+        var perm = Next();
+
+        while (perm != null)
+        {
+            Console.WriteLine(String.Join(" ", perm));
+            perm = Next();
+        }
+    }
+
+    private static char[] Next()
+    {
+        if (arr == null)
+        {
+            return null;
+        }
+
+        char[] res = new char[arr.Length];
+        Array.Copy(arr, res, arr.Length);
+
+        int i = permSwappings.Length - 1;
+
+        while (i >= 0 && permSwappings[i] == arr.Length - 1)
+        {
+            Swap(i, permSwappings[i]);
+            permSwappings[i] = i;
+            i--;
+        }
+
+        if (i < 0)
+        {
+            arr = null;
+        }
+        else
+        {
+            int prev = permSwappings[i];
+            Swap(i, prev);
+            int next = prev + 1;
+            permSwappings[i] = next;
+            Swap(i, next);
+        }
+
+        return res;
+    }
+
+    private static void Swap(int i, int v)
+    {
+        char temp = arr[i];
+        arr[i] = arr[v];
+        arr[v] = temp;
+    }
+}
---
+++ 14-Algorithms/07_COMBINATORIAL AND GREEDY ALGORITHMS - EXERCISE/CombinatorialExercises/03_IterativePermutationsWithRepetition/StartUp.cs
@@ -0,0 +1,50 @@
+﻿//DONT HAVE Iterative Version...
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var letters = Console.ReadLine().Split().ToArray();
+
+        Generate(letters, 0);
+    }
+
+    private static void Generate(string[] letters, int index)
+    {
+        if (index >= letters.Length)
+        {
+            Console.WriteLine(string.Join(" ", letters));
+        }
+        else
+        {
+            Generate(letters, index + 1);
+
+            var used = new HashSet<string> { letters[index] };
+
+            for (int i = index + 1; i < letters.Length; i++)
+            {
+                if (!used.Contains(letters[i]))
+                {
+                    used.Add(letters[i]);
+                    Swap(letters, index, i);
+                    Generate(letters, index + 1);
+                    Swap(letters, index, i);
+                }
+
+            }
+        }
+    }
+
+    private static void Swap(string[] letters, int index, int i)
+    {
+        var temp = letters[index];
+        letters[index] = letters[i];
+        letters[i] = temp;
+    }
+}
