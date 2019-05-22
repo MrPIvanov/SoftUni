98831e474114c0441029610b22eb8eb09988545a
MrPIvanov <MrPIvanov1117@gmail.com>
2019-05-22 12:04:05 +0300

    CSharp Web Basics Intro Files.

--- 14-Algorithms/17_SOLVING PRACTICAL PROBLEMS/SolvingProblemsPart01Lab/01_Elections/StartUp.cs
+++ 14-Algorithms/17_SOLVING PRACTICAL PROBLEMS/SolvingProblemsPart01Lab/01_Elections/StartUp.cs
@@ -1,7 +1,41 @@
-﻿public class StartUp
+﻿using System;
+
+public class StartUp
 {
+    private static int[] parties;
+
     public static void Main()
     {
+        var targetSum = int.Parse(Console.ReadLine());
+        var numberOfParties = int.Parse(Console.ReadLine());
 
+        parties = new int[numberOfParties];
+
+        for (int i = 0; i < numberOfParties; i++)
+        {
+            parties[i] = int.Parse(Console.ReadLine());
+        }
+
+        var resultCounter = 0;
+
+        Generate(0, 0);
+
+        Console.WriteLine(resultCounter);
+    }
+
+    private static void Generate(int indexToFill, int elementToTake)
+    {
+        if (indexToFill >= parties.Length)
+        {
+            return;
+        }
+
+        for (int i = indexToFill; i < parties.Length; i++)
+        {
+            if (true)
+            {
+
+            }
+        }
     }
 }
