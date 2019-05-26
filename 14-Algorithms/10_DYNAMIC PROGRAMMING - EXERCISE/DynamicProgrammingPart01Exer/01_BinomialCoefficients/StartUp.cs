74f57ad34ef7320d190461edc33dd288bcd1fc5c
MrPIvanov <MrPIvanov1117@gmail.com>
2019-05-26 18:24:35 +0300

    Dynamic Programming Part 01 Exercises.

--- 14-Algorithms/10_DYNAMIC PROGRAMMING - EXERCISE/DynamicProgrammingPart01Exer/01_BinomialCoefficients/StartUp.cs
+++ 14-Algorithms/10_DYNAMIC PROGRAMMING - EXERCISE/DynamicProgrammingPart01Exer/01_BinomialCoefficients/StartUp.cs
@@ -1,7 +1,40 @@
-﻿public class StartUp
+﻿using System;
+
+public class StartUp
 {
+    static long[][] coefficients;
+
     public static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
 
+        coefficients = new long[n + 1][];
+        coefficients[0] = new long[] { 1 };
+
+        long result = GetBinomialCoefficients(n, k);
+        Console.WriteLine(result);
+    }
+
+    static long GetBinomialCoefficients(int n, int k)
+    {
+        if (coefficients[n] == null)
+        {
+            coefficients[n] = new long[n + 1];
+        }
+
+        if (k < 0 || k >= coefficients[n].Length)
+        {
+            return 0;
+        }
+
+        if (coefficients[n][k] != 0)
+        {
+            return coefficients[n][k];
+        }
+
+        long coef = GetBinomialCoefficients(n - 1, k - 1) + GetBinomialCoefficients(n - 1, k);
+        coefficients[n][k] = coef;
+        return coef;
     }
 }
