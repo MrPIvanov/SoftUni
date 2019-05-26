cd6a2fc4f3fcc1089457eb1d6a5b28b8e4cf15fa
MrPIvanov <MrPIvanov1117@gmail.com>
2019-05-26 11:33:37 +0300

    Greedy Algorithms Exercises.

--- 14-Algorithms/07_COMBINATORIAL AND GREEDY ALGORITHMS - EXERCISE/GreedyAlgorithmsExercises/05_EgyptianFractions/StartUp.cs
+++ 14-Algorithms/07_COMBINATORIAL AND GREEDY ALGORITHMS - EXERCISE/GreedyAlgorithmsExercises/05_EgyptianFractions/StartUp.cs
@@ -1,7 +1,44 @@
-﻿public class StartUp
+﻿using System;
+using System.Linq;
+
+public class Program
 {
     public static void Main()
     {
+        int[] tokens = Console.ReadLine().Split('/').Select(e => int.Parse(e)).ToArray();
+        int p = tokens[0];
+        int q = tokens[1];
 
+        if (p >= q)
+        {
+            Console.WriteLine("Error (fraction is equal to or greater than 1)");
+            return;
+        }
+
+        Console.Write($"{p}/{q} = ");
+        if (q % p == 0)
+        {
+            q = q / p;
+            p = 1;
+            Console.WriteLine($"1/{q}");
+            return;
+        }
+
+        while (true)
+        {
+            int divider = (p + q) / p;
+            Console.Write($"1/{divider} + ");
+
+            p = (p * divider) - q;
+            q = q * divider;
+
+            if (q % p == 0)
+            {
+                q = q / p;
+                p = 1;
+                Console.WriteLine($"1/{q}");
+                break;
+            }
+        }
     }
 }
