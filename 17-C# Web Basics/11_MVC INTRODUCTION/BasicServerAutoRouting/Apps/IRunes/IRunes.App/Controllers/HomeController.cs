12ab475f89b50f09f391deedbe603468acb1c683
MrPIvanov <MrPIvanov1117@gmail.com>
2019-07-24 15:58:19 +0300

    MVC Intro Lecture and Lab exercises.

---
+++ 17-C# Web Basics/11_MVC INTRODUCTION/BasicServerAutoRouting/Apps/IRunes/IRunes.App/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+
+namespace IRunes.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet(Url = "/")]
+        public IHttpResponse IndexSlash(IHttpRequest httpRequest)
+        {
+            return Index(httpRequest);
+        }
+
+        public IHttpResponse Index(IHttpRequest httpRequest)
+        {
+            if (this.IsLoggedIn(httpRequest))
+            {
+                this.ViewData["Username"] = httpRequest.Session.GetParameter("username");
+
+                return this.View("Home");
+            }
+
+            return this.View();
+        }
+    }
+}
---
+++ 17-C# Web Basics/11_MVC INTRODUCTION/BasicServerAutoRouting/Apps/IRunes/IRunes.Data/DatabaseConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace IRunes.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString = @"Server=(LocalDB)\MSSQLLocalDB;Database=IRunesDB;Integrated Security=True;";
+    }
+}
