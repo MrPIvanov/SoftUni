4cf2be9e1ebdf213d99841de4d4d336f6064d885
MrPIvanov <MrPIvanov1117@gmail.com>
2019-08-09 10:09:02 +0300

    Exam Files Ready

---
+++ 17-C# Web Basics/22_RETAKE/Solution/Apps/Panda/Panda.Data/DbSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Data
+{
+    public static class DbSettings
+    {
+        public const string ConnectionString = @"Server=(LocalDB)\MSSQLLocalDB;Database=IRunesDB;Trusted_Connection=True;Integrated Security=True;";
+    }
+}
